2c5b8771ae9a9036822694a84c1131bf42974572
NguyenThu0611 <nthu061198@gmail.com>
2020-06-29 14:58:59 +0700

    bonus thay doi password chua xong

--- app/src/main/java/com/nhom08/doanlaptrinhandroid/UserHome.java
+++ app/src/main/java/com/nhom08/doanlaptrinhandroid/UserHome.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,5 +22,7 @@ public class UserHome extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+
     }
 }
